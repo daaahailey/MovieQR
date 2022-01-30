e65e9f4deadee66b42510aeb44e7a91de3d225a8
Dahye Ji <daaahailey@gmail.com>
2022-01-31 00:01:54 +0900

    ✅ added types for movie data

---
+++ src/types/MovieDataType.ts
@@ -0,0 +1,13 @@
+export interface movieDataType {
+    "genre_ids": [number];
+    id: number;
+    "original_language": string;
+    "original_title": string;
+    overview: string;
+    popularity: number;
+    "poster_path": string;
+    "release_date": string;
+    title: string;
+    "vote_average": number;
+    "vote_count": number;
+}
