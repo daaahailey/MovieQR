7be506156c70b5e912abb49f419dc0168453158f
Dahye Ji <daaahailey@gmail.com>
2022-01-30 23:39:30 +0900

    ♻️ changed useSWR for conditional fetching

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,6 +2,7 @@ import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
 import { MOVIE_SEARCH_URL } from "../constants";
 
-export const useData = (apiKey: string, path: string) => {
-    return useSWR(`${MOVIE_SEARCH_URL}${apiKey}${path}`, fetcher)
+
+export const useData = (searchQuery: string) => {
+    return useSWR(searchQuery !== "" ? `${MOVIE_SEARCH_URL}${process.env.NEXT_PUBLIC_API_KEY as string}${searchQuery}`: null, fetcher);
 }
