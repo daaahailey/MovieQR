96c2ece0dd07e217ecba17e4452acdc9086ca69d
Dahye Ji <daaahailey@gmail.com>
2022-02-02 23:59:22 +0900

    ✅  get detailed movie data using movieId and getServerSideProps

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 // API end point for movie search
 export const MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?";
+
+export const MOVIE_DETAIL_URL = "https://api.themoviedb.org/3/movie";
--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,5 +1,9 @@
-// import React from "react";
-const MovieDetail = () => {
+import React from "react";
+import { MOVIE_DETAIL_URL } from "../../constants";
+
+const MovieDetail = ({ movieData } :any) => {
+    // const id = movieId;
+    console.log(movieData) 
     return (
         <div>
             Detail page
@@ -7,6 +11,23 @@ const MovieDetail = () => {
     )
 }
 
+
+export const getServerSideProps = async ({ params }: any) => {
+    const movieId = params.id;
+    const res = await fetch(`${MOVIE_DETAIL_URL}/${movieId}?${process.env.NEXT_PUBLIC_API_KEY as string}`);
+    const movieData = await res.json();
+    // console.log(movieData)
+
+    return {
+        props: { movieData }
+    }
+}
+
+
 export default MovieDetail;
+
+
+
+
 
 
