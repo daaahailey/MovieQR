d8fca8a0ab2c0a64a54f9b32797bd154d2b3b840
Dahye Ji <daaahailey@gmail.com>
2022-02-26 23:53:54 +0900

    ✅ get movie title using Link query and useRouter

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { useState } from "react";
 import { Movies } from "./Movies";
 import { SearchResult } from "./SearchResult";
-
+import Link from "next/link";
 
 export const SearchBox = () => {
 
     const [initialValue, setInitialValue] = useState("");
-    const [value, setValue] = useState("");
-    const [click, setClick] = useState(false);
+    // const [value, setValue] = useState("");
+    // const [click, setClick] = useState(false);
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        setValue(initialValue);
-        setClick(true);
-        setInitialValue("");
-    }
+    // const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    //     event.preventDefault();
+    //     setValue(initialValue);
+    //     setClick(true);
+    //     setInitialValue("");
+    //     return value;
+    // }
+    
     // console.log(value);
     // console.log(initialValue);
 
@@ -24,9 +26,15 @@ export const SearchBox = () => {
             <label htmlFor="search">Search Contents</label>
             <input type="search" id="search" value={initialValue} placeholder="Search contents"
                 onChange={(event) => setInitialValue(event.target.value)} />
-            <button type="submit" onClick={handleClick}>Search</button>
-            {click && <SearchResult searchValue={value} isClicked={click} />}
-            <Movies input={value} />
+            <Link href={{
+              pathname: '/movie',
+              query: { title: initialValue },
+            }}>
+                <a>Search</a>
+            </Link>
+            {/* <button type="submit" onClick={handleClick}>Search</button> */}
+            {/* {click && <SearchResult searchValue={value} isClicked={click} />}
+            <Movies input={value} /> */}
         </div>
     )
 }
--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -1,11 +1,20 @@
 import { useRouter } from "next/router";
 
-const Movie = ({ movieId, clicked }: any) => {
+// const Movie = ({ movieId, clicked }: any) => {
+const Movie = () => {
     // console.log(movieId)
+
+
     const router = useRouter();
-    if(movieId) {
-        router.push({pathname: `movie/${movieId}`})
-    };
+    const { title } = router.query;
+    console.log(router)
+    console.log(title)
+
+
+
+    // if(movieId) {
+    //     router.push({pathname: `movie/${movieId}`})
+    // };
 
     return (
         <div></div>
