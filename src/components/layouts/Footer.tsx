016dcaad0520b2206c577917d97014248a9719bb
Dahye Ji <daaahailey@gmail.com>
2022-03-21 23:59:26 +0900

    💄 added basic style for footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,14 +1,40 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 export const Footer = () => {
 
     return (
-        <footer>
-            <strong>Built and designed by @daaahailey</strong>
-            <ul>
-                <li>About Movie QR</li>
-                <li>Contact us</li>
-            </ul>
-        </footer>
+        <StyledFooter>
+            <FooterStrong>Built and designed by @daaahailey</FooterStrong>
+            <FooterUl>
+                <FooterList>About Movie QR</FooterList>
+                <FooterList>Contact Us</FooterList>
+            </FooterUl>
+        </StyledFooter>
     )
 };
+
+
+const StyledFooter = styled.footer`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    padding: 2rem;
+    background-color: #272643;
+    color: #ffffff;
+`
+
+const FooterStrong = styled.strong`
+    font-weight: 700;
+    padding: 1.5rem;
+`
+
+const FooterUl = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`
+
+const FooterList = styled.li`
+    padding: 1rem;
+`
