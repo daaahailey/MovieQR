6a1a828e4abb83e89550ab95fbde342a7b6e5d99
Dahye Ji <daaahailey@gmail.com>
2022-04-07 23:40:59 +0900

    ✅ added Doughnut chart for RateChart component

--- src/components/RateChart.tsx
+++ src/components/RateChart.tsx
@@ -1,13 +1,54 @@
 import React from "react";
+import "chart.js/auto";
+import { Doughnut } from "react-chartjs-2";
+
 
 export const RateChart = ({rate}:any) => {
     // console.log(rate)
+    const movieRatePercentage = rate * 10;
+    const remainPercentage = 100 - movieRatePercentage;
+
+    const data = {
+        labels: [
+            'Rate',
+            'Remain',
+        ],
+        datasets: [{
+            data: [movieRatePercentage, remainPercentage],
+            backgroundColor: [
+            '#82FFF2',
+            '#32405D',
+            ],
+            cutout: "80%", 
+        }],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                enabled: false
+           },
+        },
+        elements: {
+            arc: {
+                borderWidth: 0
+            }
+        },
+    };
 
     return (
         <div> 
             <div> 
                 <p>Rate</p>
                 <p>{rate}</p>
+                <Doughnut
+                    data={data}
+                    options={options}
+                />
             </div>
         </div>
     )
