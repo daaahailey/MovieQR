ab1df4090e271e9958f74bc4e983d6ff6e831961
Dahye Ji <daaahailey@gmail.com>
2022-04-08 23:57:09 +0900

    💄 added style for RateChart

--- src/components/RateChart.tsx
+++ src/components/RateChart.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "chart.js/auto";
 import { Doughnut } from "react-chartjs-2";
+import styled from "@emotion/styled";
 
 
 export const RateChart = ({rate}:any) => {
@@ -41,15 +42,57 @@ export const RateChart = ({rate}:any) => {
     };
 
     return (
-        <div> 
-            <div> 
-                <p>Rate</p>
-                <p>{rate}</p>
+        <OuterContainer> 
+            <RateContainer> 
+                <RateText>{rate}</RateText>
                 <Doughnut
                     data={data}
                     options={options}
                 />
-            </div>
-        </div>
+                <Score>Rate</Score>
+            </RateContainer>
+        </OuterContainer>
     )
 }
+
+
+const OuterContainer = styled.div`
+    display: flex;
+    width: 66px;
+    height: 66px;
+    background-color: #172644;
+    border-radius: 50%;
+`
+
+const RateContainer = styled.div`
+    position: relative;
+    display: flex;
+    width: 56px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin: 0 auto;
+    
+`
+
+const Score = styled.span`
+    position: absolute;
+
+    left: 4.5rem; 
+    text-align: center;
+    font-size: 1.25rem;
+    font-weight: 700;
+    top: 50%;
+    transform: translateY(-50%);
+`
+
+const RateText = styled.p`
+    position: absolute;
+    width: 56px;
+    height: 56px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.1rem;
+    font-weight: 700;
+`
