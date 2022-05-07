5a7a90e6707cb4ab1e3a521ab20f15402b1a444d
Dahye Ji <daaahailey@gmail.com>
2022-05-07 21:33:14 +0900

    ✅ ♻️ added log out function using magic

--- src/components/layouts/Navigation.tsx
+++ src/components/layouts/Navigation.tsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
 import Image from 'next/image'
+import { useRouter } from 'next/router';
+import { magic } from "../../../lib/magic-client";
+
 
 export const Navigation = () => {
-    
+    const [userName, setUserName] = useState("");
+    const router = useRouter();
+
+    useEffect(() => {
+        // Assumes a user is already logged in
+        async function fetchData() {
+            try {
+                const { email } = await magic.user.getMetadata();
+                if(email) {
+                    setUserName(email)
+                }
+            } catch(error) {
+            // Handle errors if required!
+                console.log(error);
+            }
+        }
+        fetchData();
+
+    }, [router]);
+
+    const handleSignOut = async(e:any) => {
+        e.preventDefault();
+        try {  
+            await magic.user.logout();
+            console.log(await magic.user.isLoggedIn()); // console.log "false" when it log out
+            setUserName("");
+            router.push("/");
+            } catch(error) {
+            // Handle errors if required!
+            console.log(error);
+            router.push("/");
+            }
+    }
+
     return (
         <StyledNav>
             <MenuSection>
                 <List>
                     <Link href="/">
                         <a>
-                            <Image src="/images/logo.png" width={160} height={44} />
+                            <Image src="/images/logo.png" width={160} height={44} alt="app-logo"/>
                         </a>
                     </Link>
                 </List>
@@ -23,8 +59,19 @@ export const Navigation = () => {
             <MenuSection>
                 <List>
                     <Link href="/login">
+                        <a>{userName}</a>
+                    </Link>
+                </List>
+                <List>
+                {
+                    userName? 
+                    <Link href="/login">
+                        <a onClick={handleSignOut}>Sign Out</a>
+                    </Link> : 
+                    <Link href="/login">
                         <a>Sign In</a>
                     </Link>
+                }
                 </List>
             </MenuSection>
         </StyledNav>
@@ -45,6 +92,7 @@ const  MenuSection = styled.ul`
     display: flex;
     flex-direction: row;
     align-items: center;
+
 `
 
 const List = styled.li`
