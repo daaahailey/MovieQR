2164f4a6db683193104f90be73dfb328980a56f9
Dahye Ji <daaahailey@gmail.com>
2022-05-08 23:53:15 +0900

    ✅  created PopularMovies, PopularMovieCard components

--- src/components/PopularMovieCard.tsx
+++ src/components/PopularMovieCard.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router';
+import styled from "@emotion/styled";
+
 
 export const PopularMovieCard = (movieItems : any) => {
 
@@ -8,19 +10,27 @@ export const PopularMovieCard = (movieItems : any) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original";
     const router = useRouter();
 
-    console.log(movieItems)
+    // console.log(movieItems)
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
         router.push({pathname: `movie/${id}`})
     }
 
     return (
-        <article onClick={handleClick}>
+        <PopularMovieCardItem  onClick={handleClick}>
             {/* <h2>Popular Now</h2> */}
             { movieItems.movieItems["poster_path"] === null ? <Image src="/images/movie_fallback.png" alt={`${movieItems.title}`} width={200} height={300} />
             : <Image src={`${BASE_URL}${poster_path}`} alt={`${movieItems.title}`} width={200} height={300}/> } 
-            <h3>{original_title}</h3>
-            <p>{orderedDate}</p>
-        </article>
+            {/* <h3>{original_title}</h3>
+            <p>{orderedDate}</p> */}
+        </PopularMovieCardItem>
     )
 }
+
+const PopularMovieCardItem = styled.article`
+    height: 100%;
+    // height: 320px;
+    // width: 11rem;
+    // height: 16rem;
+    // padding: 0.5rem;
+`
--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -2,6 +2,18 @@ import { useRandomMovie } from "../hooks/useRandomMovie";
 import { Error } from "./Error";
 import { Loading } from "./Loading";
 import { PopularMovieCard } from "./PopularMovieCard";
+import styled from "@emotion/styled";
+
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
 
 
 export const PopularMovies = () => {
@@ -12,15 +24,76 @@ export const PopularMovies = () => {
     if (error) return <Error />
     if(data) {
         movieData = data.results;
-        
+
         return (
-            <section>
-                <h2 className="text-hide">Currently Popular Movies</h2>
-                <p>Popular Now</p>
-                {movieData.map((item : any) => <PopularMovieCard key={`movie-data${item.id}`} movieItems={item} />)}
-            </section>
+            <>
+                <PopularMovieSection>
+                    <PopularSectionTitle>Popular Now</PopularSectionTitle>
+                    <Swiper
+                        modules={[Pagination, Navigation]}
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        // slidesPerGroup={3}
+                        loop={true}
+                        loopFillGroupWithBlank={true}
+                        pagination={{
+                        clickable: true,
+                        }}
+
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 2,
+                                slidesPerGroup: 2,
+                            },
+                            540: {
+                                slidesPerView: 3,
+                                slidesPerGroup: 3,
+                            },
+                            720: {
+                                slidesPerView: 4,
+                                slidesPerGroup: 4,
+                            },
+                            1024: {
+                                slidesPerView: 6,
+                                slidesPerGroup: 6,
+                            },
+                            1440: {
+                                slidesPerView: 9,
+                                slidesPerGroup: 9,
+                            },
+                        }}
+                        navigation={true}
+                        // modules={[Pagination, Navigation]}
+                        className="mySwiper"
+                    >         
+                    <h2 className="text-hide">Currently Popular Movies</h2>
+                    <div>
+                        {movieData.map((item : any) => 
+                            <SwiperSlide key={`movie-data${item.id}`}> 
+                                <PopularMovieCard key={`movie-data${item.id}`} movieItems={item} />
+                            </SwiperSlide> 
+                        )}
+                     </div>
+                    </Swiper> 
+                </PopularMovieSection>        
+            </>
         )
     } else if(!data) {
         return <Loading />
     }
 }
+
+
+const PopularMovieSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    padding: 1.2rem;
+`
+
+const PopularSectionTitle =styled.p`
+    font-size: 2rem;
+    font-weight: 700;
+    margin-top: 1.2rem;
+    margin-bottom: 0.8rem;
+`
+
