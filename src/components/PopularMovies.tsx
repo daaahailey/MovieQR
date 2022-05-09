0a3136446a32d8249663ce2604f526222aed389d
Dahye Ji <daaahailey@gmail.com>
2022-05-09 10:01:27 +0900

    ✅ added PopularMovies to home

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -1,11 +1,9 @@
 import { useRandomMovie } from "../hooks/useRandomMovie";
 import { Error } from "./Error";
-import { Loading } from "./Loading";
 import { PopularMovieCard } from "./PopularMovieCard";
 import styled from "@emotion/styled";
-
-import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { movieDataType } from "../types/MovieDataType";
 
 // Import Swiper styles
 import "swiper/css";
@@ -15,72 +13,68 @@ import "swiper/css/navigation";
 // import required modules
 import { Pagination, Navigation } from "swiper";
 
-
 export const PopularMovies = () => {
     const baseURL = "https://api.themoviedb.org/3/movie/popular";
     const {data, error} : any = useRandomMovie(baseURL);
     let movieData;
 
     if (error) return <Error />
+    if(!data) {
+        return <Error />
+    }
     if(data) {
         movieData = data.results;
-
-        return (
-            <>
-                <PopularMovieSection>
-                    <PopularSectionTitle>Popular Now</PopularSectionTitle>
-                    <Swiper
-                        modules={[Pagination, Navigation]}
-                        slidesPerView={1}
-                        spaceBetween={30}
-                        // slidesPerGroup={3}
-                        loop={true}
-                        loopFillGroupWithBlank={true}
-                        pagination={{
-                        clickable: true,
-                        }}
-
-                        breakpoints={{
-                            0: {
-                                slidesPerView: 2,
-                                slidesPerGroup: 2,
-                            },
-                            540: {
-                                slidesPerView: 3,
-                                slidesPerGroup: 3,
-                            },
-                            720: {
-                                slidesPerView: 4,
-                                slidesPerGroup: 4,
-                            },
-                            1024: {
-                                slidesPerView: 6,
-                                slidesPerGroup: 6,
-                            },
-                            1440: {
-                                slidesPerView: 9,
-                                slidesPerGroup: 9,
-                            },
-                        }}
-                        navigation={true}
-                        // modules={[Pagination, Navigation]}
-                        className="mySwiper"
-                    >         
-                    <h2 className="text-hide">Currently Popular Movies</h2>
-                    <div>
-                        {movieData.map((item : any) => 
-                            <SwiperSlide key={`movie-data${item.id}`}> 
-                                <PopularMovieCard key={`movie-data${item.id}`} movieItems={item} />
-                            </SwiperSlide> 
-                        )}
-                     </div>
-                    </Swiper> 
-                </PopularMovieSection>        
-            </>
-        )
-    } else if(!data) {
-        return <Loading />
     }
+    return (
+        <PopularMovieSection>
+            <PopularSectionTitle>Popular Now</PopularSectionTitle>
+            <Swiper
+                modules={[Pagination, Navigation]}
+                slidesPerView={1}
+                spaceBetween={30}
+                // slidesPerGroup={3}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                clickable: true,
+                }}
+
+                breakpoints={{
+                    0: {
+                        slidesPerView: 2,
+                        slidesPerGroup: 2,
+                    },
+                    540: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 3,
+                    },
+                    720: {
+                        slidesPerView: 4,
+                        slidesPerGroup: 4,
+                    },
+                    1024: {
+                        slidesPerView: 6,
+                        slidesPerGroup: 6,
+                    },
+                    1440: {
+                        slidesPerView: 9,
+                        slidesPerGroup: 9,
+                    },
+                }}
+                navigation={true}
+                className="mySwiper"
+            >         
+            <h2 className="text-hide">Currently Popular Movies</h2>
+            <div>
+                {movieData.map((item : any) => 
+                    <SwiperSlide key={`slide-${item.id}`}> 
+                        <PopularMovieCard key={`movie-data${item.id}`} movieItems={item} />
+                    </SwiperSlide> 
+                )}
+                </div>
+            </Swiper> 
+        </PopularMovieSection>
+    )
 }
 
 
