0f844b4ea87aaca06dd2bc0f8897e6d0232ff95f
Dahye Ji <daaahailey@gmail.com>
2022-05-12 15:56:31 +0900

    ✅ set cookie using token from login api

---
+++ lib/cookie.tsx
@@ -0,0 +1,14 @@
+import cookie from "cookie";
+
+
+const MAX_AGE =  7 * 24 * 60 * 60; // 7days in sec.
+export const setTokenCookie = (token:any, res:any) => {
+    const  setCookie = cookie.serialize("token", token, {
+        maxAge: MAX_AGE,
+        expires: new Date(Date.now() + MAX_AGE * 1000), // in 7days from current date
+        // secure: process.env.NODE_ENV = "production",
+        path: "/",
+    });
+    res.setHeader("Set-Cookie", setCookie);
+
+}
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,6 +1,7 @@
 import { magicAdmin } from "../../../lib/magic";
 import jwt from "jsonwebtoken";
 import { isNewUser, createNewUser } from "../../../lib/db/hasura";
+import { setTokenCookie } from "../../../lib/cookie";
 
 export default async function login(req :any , res :any) {
     if(req.method === "POST") {
@@ -28,19 +29,14 @@ export default async function login(req :any , res :any) {
             // process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET as string);
             process.env.JWT_SECRET as string);
 
-            // check if user exists
+            // check if a user exists
             const isNewUserQuery = await isNewUser(token, metadata.issuer);
 
-            if(isNewUserQuery) {
-                // create a new user 
-                const createNewUserMutation = await createNewUser(token, metadata);
-                console.log({ createNewUserMutation });
-
-                res.send({ done: true, message: "a new user" }); 
-            } else {
-                res.send({ done: true, message: "not a new user" }); 
-            }
-
+            // if it's a new user create a new user 
+            isNewUserQuery && (await createNewUser(token, metadata));
+            // set the cookie
+            setTokenCookie(token, res);     
+            res.send({ done: true, message: "a new user" }); 
         } catch(error) {
             console.log("Something went wrong logging in", error);
             res.status(500).send({ done: false });
