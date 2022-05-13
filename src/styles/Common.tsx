09d8b809cf6e28c72948d75e420ee7495c717fc1
Dahye Ji <daaahailey@gmail.com>
2022-05-13 17:46:25 +0900

    💄 created Common file for style

---
+++ src/styles/Common.tsx
@@ -0,0 +1,22 @@
+export const Common = {
+    colors: {
+        backgroundBlack: "#000000",
+        backgroundGray: "#141010",
+        point: "#f70776",
+        text: "#ffffff",
+    },
+    fontSize: {
+        extraSmall: "0.75rem",
+        small: "0.95rem",
+        basic: "1rem",
+        medium: "1.5rem", 
+        large: "2rem",
+        extraLarge: "3rem",
+    },
+    fontWeight: {
+        basic: "400",
+        medium: "600",
+        bold: "800",
+        extraBold: "900",
+    },
+}
