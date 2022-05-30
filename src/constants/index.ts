f85dca8f3721fb21a370c62d471f7fa26d64ced4
Dahye Ji <daaahailey@gmail.com>
2022-05-31 00:50:04 +0900

    ✅ added constant for popular movies url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 // API end point for movie search
 export const MOVIE_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?";
-
+export const POPULAR_MOVIES_URL = "https://api.themoviedb.org/3/movie/popular";
 export const MOVIE_DETAIL_URL = "https://api.themoviedb.org/3/movie";
