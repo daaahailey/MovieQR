25932f5c698f21f741ae0f7b131e36a6f7dbc99e
Dahye Ji <daaahailey@gmail.com>
2022-06-07 16:31:17 +0900

    ♻️ save cookie and didToken using context api when user has logged in(if there's cookie in header)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,43 @@
+import React, { useState, useEffect } from "react";
 import type { NextPage } from 'next';
 import Head from 'next/head'
 import { SearchContainer } from '../components/SearchContainer';
 import { PopularMovies } from '../components/PopularMovies';
 import { useAuth } from '../../context/AuthContext';
-import { useEffect } from 'react';
+import { magic } from "../../lib/magic-client";
+import jwt from "jsonwebtoken";
 
-const Home: NextPage = (token:any) => {
-  const { login, saveCookie } = useAuth();
+const Home: NextPage = (token:any, dToken:any) => {
   const userToken = token.token;
-  // console.log(userToken)
+  const { saveCookie, saveDidToken } = useAuth();
+  const [userEmail, setUserEmail] = useState("");
+
+  interface JwtPayload {
+  issuer: string;
+  email: string;
+  }
 
   useEffect(() => {
-    if(token) {
-      saveCookie(userToken)
-    } else if(!token) {
-      saveCookie("");
+    const getMagicDidToken = async () => {
+      try {
+          if(magic) {
+            const dToken = await magic.user.getIdToken(); 
+            saveDidToken(dToken);
+          }
+      } catch(error) {
+          console.log(error)
+      }
     }
-  })
+
+    if(userToken) {
+      const decodedToken = jwt.verify(userToken, process.env.NEXT_PUBLIC_JWT_SECRET as string) as JwtPayload;  
+      const email = decodedToken.email;
+      setUserEmail(email) // set email using cookie from header
+      saveCookie(userToken);
+      getMagicDidToken(); // get did token from magic client and saveDidToken
+    }
+  }, [userToken])
+
 
   return (
     <>
@@ -35,6 +56,8 @@ const Home: NextPage = (token:any) => {
 
 export default Home;
 
+
+
 export async function getServerSideProps(context:any) {
   const cookies = context.req.headers.cookie;
   let token = null;
@@ -42,6 +65,7 @@ export async function getServerSideProps(context:any) {
   if(cookies) {
     token = cookies.split("token=")[1];
   }
+
   return {
     props: {
       token,
