01a9020a52768bef342b9c01e7ce9a32cf25ea28
Dahye Ji <daaahailey@gmail.com>
2022-06-08 19:32:31 +0900

    ✅ created quotes and reviews pages

---
+++ src/pages/quotes.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Quotes: NextPage = () => {
+    return (
+        <main>
+            <p>Quotes page</p>
+        </main>
+    )
+}
+
+export default Quotes;
---
+++ src/pages/reviews.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Reviews: NextPage = () => {
+    return (
+        <main>
+            <p>Reviews page</p>
+        </main>
+    )
+}
+
+export default Reviews;
