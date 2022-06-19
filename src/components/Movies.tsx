01d2fc6f48c099e89902ac5b1c78cf2cf0a462c5
Dahye Ji <daaahailey@gmail.com>
2022-06-20 03:44:23 +0900

    ♻️ handle when there isn't search result for movie as well (added not found message) / also, moved fetching data to movie/index from Movies component to check search result beforehand

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,25 +1,12 @@
 import React from "react";
-import { useData } from "../hooks/useData";
-import { Error } from "./Error";
-import { Loading } from "./Loading";
 import MovieCard from "./MovieCard";
 
-
-export const Movies = ({title}: any) => {
-    // console.log(title)
-    let searchQuery;
-    title === "" ? searchQuery = "" : searchQuery = title;
-    const { data, error }: any = useData(searchQuery);
-    let receivedData;
-
-    if (error) return <Error/>
-    if(data) {
-        receivedData = data.results;
-        return receivedData.map((item:any) => <MovieCard key={`movie-data${item.id}`} movieData={item}  />)
-    } else if(!data) {
-        if(title !== "") {
-            return <Loading />
+export const Movies = ({data}:any) => {   
+    return (
+    <>
+        { data &&
+            data.map((item:any) => <MovieCard key={`movie-data${item.id}`} movieData={item} />) 
         }
-        return <div>default display</div>
-    }
+    </>
+    )
 }
--- src/pages/movie/index.tsx
+++ src/pages/movie/index.tsx
@@ -1,19 +1,48 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import { Movies } from "../../components/Movies";
 import { jsx, css } from '@emotion/react';
-
+import { useData } from "../../hooks/useData";
+import { Error } from "../../components/Error";
+import Link from "next/link";
+import { Common } from "../../styles/common";
 
 const Movie = () => {
     const router = useRouter();
     const { title } = router.query;
+    let searchQuery:any;
+    title === "" ? searchQuery = "" : searchQuery = title;
+    const { data, error }: any = useData(searchQuery);
+    const [ isNothingFound, setIsNothingFound ] = useState(false);
+
+    useEffect(() => {
+        if(data) {
+            let receivedData = data.results;
+            if(receivedData.length === 0) {
+                setIsNothingFound(true);
+            }
+        }
+    }, [data]);
+
 
     return (
-        <main>
-            <section css={StyledSectionForMovieCard}>
+        <main css={Main}>
+            <section css={isNothingFound ? StyledSectionDefault : StyledSectionForMovieCard}>
                 <h2 className="text-hide">Movie cards by search result</h2>
-                <Movies title={title}/>
+                <Movies title={title} /> 
+                { data ? 
+                    data.results.length > 0 ?
+                        <Movies title={title} data={data.results}/> :
+                        <div css={NotFoundMessage}>
+                            <p>Search result of <span css={Title}>{title}</span></p>
+                            <p>Nothing Found. Please search something else.</p>
+                            <Link href="/"> 
+                                <a css={GoBack}>Go back to main page</a>
+                            </Link>
+                        </div> 
+                    : <div>Loading...</div>}
             </section>
         </main>
     )
@@ -21,6 +50,9 @@ const Movie = () => {
 
 export default Movie;
 
+const Main = css`
+    min-height: 80vh;
+`
 
 const StyledSectionForMovieCard = css`
     display: grid;
@@ -32,9 +64,32 @@ const StyledSectionForMovieCard = css`
     padding: 5rem 3rem;
 
     @media (max-width: 490px) {
-        padding: 3rem 1.5rem;
+        padding: 3rem 1.5rem ;
     }
     @media (max-width: 400px) {
-        padding: 2.4rem 1rem;
+        padding: 2.4rem 1rem ;
     }
+`
+
+const StyledSectionDefault = css`
+    position: absolute;
+    width: 100%;
+    right: 50%;
+    top: 50%;
+    transform: translate(50%, -50%);
+    text-align: center;
+    padding: 2rem;
+`
+
+const NotFoundMessage = css`
+    line-height: 1.5rem;
+`
+const Title = css`
+    font-weight: ${Common.fontWeight.medium};
+`
+
+const GoBack = css`
+    cursor: pointer;
+    font-weight: ${Common.fontWeight.medium};
+    color: ${Common.colors.point};
 `
