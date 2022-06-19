31e6cc64fce91ba180ecd21132852351257baa97
Dahye Ji <daaahailey@gmail.com>
2022-06-20 03:49:14 +0900

    ⚰️ removed unsed file

--- src/components/SearchResult.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const SearchResult = ({ searchValue, isClicked }: any) => {
-    return (
-        isClicked && searchValue ? 
-        <p>{searchValue}</p>
-        : <p>default display (when search value is empty)</p>
-    )
-}
